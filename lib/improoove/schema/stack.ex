9272436bdc4e3746c9f87c6c56471a2cd2962752
Gyeongmin Park <ddogi1513@naver.com>
2023-01-21 15:32:09 +0900

    delete reminders and columns move to stacks

--- lib/improoove/schema/stack.ex
+++ lib/improoove/schema/stack.ex
@@ -1,16 +1,17 @@
 defmodule Improoove.Schema.Stack do
-  alias Improoove.Schema.Reminder
+  alias Improoove.Schema.Tag
   use Ecto.Schema
   import Ecto.Changeset
 
   schema "stacks" do
     field :description, :string
     field :project_id, :integer
-    field :type, :string
+    field :type, Ecto.Enum, values: [:FEEDBACK, :LOG]
     field :user_id, :integer
+    field :is_sent, :boolean
+    field :sent_at, :utc_datetime
 
-    has_many :reminder, Reminder
-    has_many :tag, Tag
+    #_has_many :tag, Tag, where: [type: :LOG], foreign_key: :log_id, references: :id, on_delete: :delete_all
     timestamps([{:inserted_at, :created_at}])
   end
 
