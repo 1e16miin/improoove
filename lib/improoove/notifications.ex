49b3a8aaac52ac89e3bac75a6b66a5c45b6f39f6
Gyeongmin Park <42409229+1e16miin@users.noreply.github.com>
2023-01-27 18:09:23 +0900

    remove-unused-ns (#3)

--- lib/improoove/notifications.ex
+++ lib/improoove/notifications.ex
@@ -1,46 +1,46 @@
-defmodule Improoove.Notifications do
-  @moduledoc """
-  The Accounts context.
-  """
+# defmodule Improoove.Notifications do
+#   @moduledoc """
+#   The Accounts context.
+#   """
 
-  import Ecto.Query, warn: false
-  alias Improoove.Repo
+#   import Ecto.Query, warn: false
+#   alias Improoove.Repo
 
-  alias Improoove.Schema.Device
+#   alias Improoove.Schema.Device
 
-  @doc """
-  Gets a single user.
+#   @doc """
+#   Gets a single user.
 
-  Raises `Ecto.NoResultsError` if the User does not exist.
+#   Raises `Ecto.NoResultsError` if the User does not exist.
 
-  ## Examples
+#   ## Examples
 
-      iex> get_user!(123)
-      %User{}
+#       iex> get_user!(123)
+#       %User{}
 
-      iex> get_user!(456)
-      ** (Ecto.NoResultsError)
+#       iex> get_user!(456)
+#       ** (Ecto.NoResultsError)
 
-  """
-  def get_devices_by_user_id(user_id) do
-    Repo.all(Device, user_id: user_id)
-  end
+#   """
+#   def get_devices_by_user_id(user_id) do
+#     Repo.all(Device, user_id: user_id)
+#   end
 
-  @doc """
-  Creates a user.
+#   @doc """
+#   Creates a user.
 
-  ## Examples
+#   ## Examples
 
-      iex> create_user(%{field: value})
-      {:ok, %User{}}
+#       iex> create_user(%{field: value})
+#       {:ok, %User{}}
 
-      iex> create_user(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
+#       iex> create_user(%{field: bad_value})
+#       {:error, %Ecto.Changeset{}}
 
-  """
-  def create_device(attrs) do
-    %Device{}
-    |> Device.changeset(attrs)
-    |> Repo.insert()
-  end
-end
+#   """
+#   def create_device(attrs) do
+#     %Device{}
+#     |> Device.changeset(attrs)
+#     |> Repo.insert()
+#   end
+# end
