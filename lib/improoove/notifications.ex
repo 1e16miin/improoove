79f1230e2f62bfdfa7ba69038f42e052fbfb3c84
Gyeongmin Park <ddogi1513@naver.com>
2023-02-05 15:19:03 +0900

    comment out

--- lib/improoove/notifications.ex
+++ lib/improoove/notifications.ex
@@ -3,9 +3,9 @@
 #   The Accounts context.
 #   """
 
-  import Ecto.Query, warn: false
-  alias Improoove.Stacks
-  alias Improoove.Repo
+#   import Ecto.Query, warn: false
+#   alias Improoove.Stacks
+#   alias Improoove.Repo
 
 #   alias Improoove.Schema.Device
 
@@ -14,7 +14,7 @@
 
 #   Raises `Ecto.NoResultsError` if the User does not exist.
 
-#   ## Examples
+#   Examples
 
 #       iex> get_user!(123)
 #       %User{}
@@ -22,18 +22,18 @@
 #       iex> get_user!(456)
 #       ** (Ecto.NoResultsError)
 
-  """
-  def get_devices_by_user_id(user_id) do
-    Repo.all(Device, user_id: user_id)
-  end
+#   """
+#   def get_devices_by_user_id(user_id) do
+#     Repo.all(Device, user_id: user_id)
+#   end
 
-  def push_notifications(user_id, msg) do
-    device_tokens =
-      get_devices_by_user_id(user_id)
-      |> Enum.map(& &1["token"])
+#   def push_notifications(user_id, msg) do
+#     device_tokens =
+#       get_devices_by_user_id(user_id)
+#       |> Enum.map(& &1["token"])
 
-    Pigeon.FCM.Notification.new(device_tokens, msg)
-  end
+#     Pigeon.FCM.Notification.new(device_tokens, msg)
+#   end
 
 #   @doc """
 #   Creates a user.
@@ -46,12 +46,10 @@
 #       iex> create_user(%{field: bad_value})
 #       {:error, %Ecto.Changeset{}}
 
-  """
-  def create_device(user_id, attrs) do
-    %Device{}
-    |> Map.put(:user_id, user_id)
-    |> IO.inspect()
-    |> Device.changeset(attrs)
-    |> Repo.insert()
-  end
-end
+#   """
+#   def create_device(attrs) do
+#     %Device{}
+#     |> Device.changeset(attrs)
+#     |> Repo.insert()
+#   end
+# end
